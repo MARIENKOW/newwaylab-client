94cea0cf0bfe7b8f6230c533530bd833bd94b513
root <root@srv663200.hstgr.cloud>
2024-12-11 14:52:27 +0000

    fix

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,6 +2,7 @@ const config = {
    CLIENT_API: "https://newwaylab.com.ua",
    // CLIENT_API: "https://newwaylab.com.ua",
    SERVER_API: "https://newwaylab.com.ua/api",
+   //SERVER_API: "http://141.136.44.10/api",
 };
 
 export default config;
