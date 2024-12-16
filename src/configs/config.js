a848b8369b016e4df2905856c043ef6bce18e9bc
Denis Marienkow <den.marienkow@gmail.com>
2024-12-16 22:57:00 +0200

    Delete src/configs/config.js

--- src/configs/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-   CLIENT_API: "https://newwaylab.com.ua",
-   // CLIENT_API: "https://newwaylab.com.ua",
-   SERVER_API: "https://newwaylab.com.ua/api",
-   //SERVER_API: "http://141.136.44.10/api",
-};
-
-export default config;
